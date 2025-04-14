597ac40bd9b927dbc0193187f02cc4c9fec92c3a
SweetBoba <>
2025-04-14 17:35:04 -0400

    fix: updated bridge to expose the functions from the main process and also the package-lock for the new dependency

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -6,4 +6,16 @@ function send(channel: string, message: string) {
   return ipcRenderer.invoke(channel, message);
 }
 
-export {sha256sum, versions, send};
+function setSecret(key: string, value: string) {
+  return ipcRenderer.invoke("secrets:set", key, value);
+}
+
+function getSecret(key: string) {
+  return ipcRenderer.invoke("secrets:get", key);
+}
+
+function deleteSecret(key: string) {
+  return ipcRenderer.invoke("secrets:delete", key);
+}
+
+export { sha256sum, versions, send, setSecret, getSecret, deleteSecret };
